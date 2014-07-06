9d7c0f468b33271b151b4b0a71456c60584b2754
Adam Mayer <phooky@gmail.com>
2014-07-05 22:15:33 -0400

    adding z80sim code to build

--- emulator/qt-vt100/qt-vt100.pro
+++ emulator/qt-vt100/qt-vt100.pro
@@ -14,9 +14,23 @@ TEMPLATE = app
 
 SOURCES += main.cpp\
         mainwindow.cpp \
-    simthread.cpp
+    simthread.cpp \
+    8080/sim1.c \
+    8080/sim1a.c \
+    8080/sim2.c \
+    8080/sim3.c \
+    8080/sim4.c \
+    8080/sim5.c \
+    8080/sim6.c \
+    8080/sim7.c \
+    8080/simfun.c \
+    8080/simglb.c \
+    8080/simint.c \
+    8080/iosim.c
 
 HEADERS  += mainwindow.h \
-    simthread.h
+    simthread.h \
+    8080/sim.h \
+    8080/simglb.h
 
 FORMS    += mainwindow.ui
--- emulator/qt-vt100/simthread.cpp
+++ emulator/qt-vt100/simthread.cpp
@@ -1,4 +1,12 @@
 #include "simthread.h"
+#include "8080/sim.h"
+#include "8080/simglb.h"
+#include <stdio.h>
+
+extern "C" {
+extern void int_on(void), int_off(void);
+extern void init_io(void), exit_io(void);
+};
 
 SimThread::SimThread(QObject *parent) :
     QThread(parent)
@@ -6,10 +14,36 @@ SimThread::SimThread(QObject *parent) :
 }
 
 void SimThread::run() {
+    i_flag = 1;
+    f_flag = 10;
+    m_flag = 0;
+    tmax = f_flag*10000;
+    cpu = I8080;
     quint8 leds = 0;
+    printf("\nRelease %s, %s\n", RELEASE, COPYR);
+    if (f_flag > 0)
+        printf("\nCPU speed is %d MHz\n", f_flag);
+    else
+        printf("\nCPU speed is unlimited\n");
+#ifdef	USR_COM
+    printf("\n%s Release %s, %s\n", USR_COM, USR_REL, USR_CPR);
+#endif
+    fflush(stdout);
+
+    wrk_ram	= PC = ram;
+    STACK = ram + 0xffff;
+    if (cpu == I8080)	/* the unused flag bits are documented for */
+        F = 2;		/* the 8080, so start with bit 1 set */
+    memset((char *)	ram, m_flag, 65536);
+    int_on();
+    init_io();
+
     while (1) {
         msleep(500);
         leds = (leds+1)%8;
         emit outKbdStatus(leds);
     }
+
+    exit_io();
+    int_off();
 }
